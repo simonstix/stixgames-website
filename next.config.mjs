d47d3a316764754b9a86500f5d4175171979a06c
Simon Stix <simon.stix@gmail.com>
2024-10-04 13:30:30 +0200

    Remove automatic redirect to support github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/tile-composer/documentation/:slug*",
-        destination: "https://docs.stixgames.com/tile-composer",
-        permanent: true,
-      },
-      {
-        source: "/grassshader/documentation/:slug*",
-        destination: "https://docs.stixgames.com/directx-11-grass-shader",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
