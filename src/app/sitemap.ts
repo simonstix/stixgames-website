86b85033bddede758811638bf4cb1e45b8bf8dc6
Simon Stix <simon.stix@gmail.com>
2024-10-12 16:26:53 +0200

    Fix links in the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: "https://stixgames.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/grass-shader",
+      url: "https://stixgames.com/grassshader",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://acme.com/tile-composer",
+      url: "https://stixgames.com/tile-composer",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
     },
     {
-      url: "https://acme.com/contact",
+      url: "https://stixgames.com/contact",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
